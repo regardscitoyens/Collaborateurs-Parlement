72d0de0d637ef062fb974dba28581ffb78e2537e
RouxRC <b.ooghe@gmail.com>
2015-05-14 03:42:32 +0200

    add split collabs in sex/name/surname

--- bin/convert.py
+++ bin/convert.py
@@ -24,17 +24,20 @@ elif "collaborateurs_senateurs" in filepath:
 with open("data/senateurs.json", 'r') as f:
     senateurs = [p["senateur"] for p in json.load(f)['senateurs']]
 
-re_particule = re.compile(r"^(.*)\s+(d[eu'\sla]+)$")
+Mme = r"^M[.mle]+\s+"
+clean_Mme = re.compile(Mme)
+
+particule = r"d[euils'\s]+"
+re_particule = re.compile(r"^(.*)\s+(%s)$" % particule)
 clean_part = lambda x: re_particule.sub(r'\2 \1', x).replace("' ", "'").replace('  ', ' ')
 
-clean_Mme = re.compile(r"^M[.mle]+\s+")
-
 re_clean_bal = re.compile(r'<[^>]+>')
+re_clean_dash = re.compile(r'\s*-\s*')
 re_clean_spaces = re.compile(r'\s+')
-clean = lambda x: re_clean_spaces.sub(' ', re_clean_bal.sub('', x)).strip()
+clean = lambda x: re_clean_spaces.sub(' ', re_clean_dash.sub('-', re_clean_bal.sub('', x))).strip()
 
 regexps = [(re.compile(r), s) for r, s in [
-    (u'[àÀ]', 'a'),
+    (u'[àÀâÂ]', 'a'),
     (u'[éÉèÈêÊëË]', 'e'),
     (u'[îÎïÏ[]', 'i'),
     (u'[ôÔöÔ]', 'o'),
@@ -51,25 +54,38 @@ checker = lambda x: clean(clean_accents(x)).lower().strip()
 
 reorder = lambda p: clean_part(checker("%s %s" % (p['nom_de_famille'], p['prenom'])))
 
+maj = ur"A-ZÀÂÉÈÊËÎÏÔÖÙÛÜÇ"
+re_name = re.compile(ur"(%s)((?:(?:%s)?[%s'\-]+\s+)+)([%s][a-zàâéèêëîïôöùûüç].*)$" % (Mme, particule, maj, maj))
+
+def split_name(name):
+    match = re_name.search(name.decode('utf-8'))
+    if not match:
+        sys.stderr.write("WARNING: could not split name %s\n" % name)
+        return name, "", ""
+    sexe = "H" if "." in match.group(1) else "F"
+    nom = match.group(2).strip()
+    prenom = match.group(3).strip()
+    return nom, prenom, sexe
+
+def split_collab(record):
+    record[5], record[6], record[7] = split_name(record[4])
+
 def find_parl(record):
     nom = checker(clean_Mme.sub('', record[0]))
     nom = nom.replace("conway-mouret", "conway mouret")
     nom = nom.replace("deromedi jacqueline", "deromedi jacky")
     nom = nom.replace("yonnet-salvator evelyne", "yonnet evelyne")
-    good = None
+    record[1], record[2], record[3] = split_name(record[0])
     for parl in senateurs:
         if reorder(parl) == nom:
-            good = parl
-            break
-    if not good:
-        sys.stderr.write("Could not find %s\n" % nom)
-        return
-    record[0] = good["nom"].encode('utf-8')
-    record[1] = good["nom_de_famille"].encode('utf-8')
-    record[2] = good["prenom"].encode('utf-8')
-    record[3] = good["sexe"].encode('utf-8')
-    record[7] = good["url_nossenateurs"].encode('utf-8')
-    record[8] = good["url_institution"].encode('utf-8')
+            record[0] = parl["nom"]
+            #record[1] = parl["nom_de_famille"]
+            #record[2] = parl["prenom"]
+            #record[3] = parl["sexe"]
+            record[8] = parl["url_nossenateurs_api"].replace('json', 'xml')
+            record[9] = parl["url_institution"]
+            return
+    sys.stderr.write("Could not find %s\n" % nom)
 
 # TODO Split collabs in nom/prénom/sexe
 
@@ -79,8 +95,8 @@ leftvals = {}
 maxtop = 0
 maxleft = 0
 results = []
-headers = ['sénateur', 'nom_sénateur', 'prénom_sénateur', 'sexe_sénateur', 'nom_collaborateur', 'prénom_collaborateur', 'sexe_collaborateur', 'url_nossénateurs', 'url_sénat']
-record = ["", "", "", "", "", "", "", "", ""]
+headers = ['sénateur', 'nom_sénateur', 'prénom_sénateur', 'sexe_sénateur', 'collaborateur', 'nom_collaborateur', 'prénom_collaborateur', 'sexe_collaborateur', 'url_api_nossénateurs', 'url_sénat']
+record = ["", "", "", "", "", "", "", "", "", ""]
 re_line = re.compile(r'<page number|text top="(\d+)" left="(\d+)"[^>]*font="(\d+)">(.*)</text>', re.I)
 for line in (xml).split("\n"):
     #print >> sys.stderr, "DEBUG %s" % line
@@ -121,16 +137,21 @@ for line in (xml).split("\n"):
             record[idx], record[idx2] = val.split(" Mme ")
             record[idx2] = "Mme " + record[idx2]
             find_parl(record)
+            split_collab(record)
             results.append(list(record))
             continue
         record[idx] = val
         if senateursfirst:
             find_parl(record)
+        else:
+            split_collab(record)
     else:
         idx = 4 if senateursfirst else 0
         record[idx] = clean(text)
         if not senateursfirst:
             find_parl(record)
+        else:
+            split_collab(record)
         results.append(list(record))
 
 if not drawMap:
@@ -138,6 +159,8 @@ if not drawMap:
     for i in results:
         for j in range(len(i)):
             i[j] = clean(i[j])
+            try:    i[j] = i[j].encode('utf-8')
+            except: pass
         print ",".join([str(i[a]) if isinstance(i[a], int) else "\"%s\"" % i[a].replace('"', '""') for a,_ in enumerate(i)])
 
 else:
