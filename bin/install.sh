09aeda6ff7f506fc0cfac12e96eafeee21cfe996
RCbot <regardscitoyens@gmail.com>
2015-05-15 01:43:37 +0200

    missing distribute on debian

--- bin/install.sh
+++ bin/install.sh
@@ -5,6 +5,7 @@ sudo pip install virtualenv virtualenvwrapper
 source $(which virtualenvwrapper.sh)
 mkvirtualenv --no-site-packages collabs
 workon collabs
+easy_install -U distribute
 pip install -r requirements.txt
 add2virtualenv .
 deactivate
