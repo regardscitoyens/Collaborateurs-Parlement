dce90a682e03a2edbb03aa21906ebc04a7512f2c
RouxRC <b.ooghe@gmail.com>
2017-09-24 12:39:28 +0200

    fix autoupdate totaux

--- bin/backlog_revisions.sh
+++ bin/backlog_revisions.sh
@@ -2,7 +2,12 @@
 
 badcommits="\(bcb0b8de41ba7223fc12fd66f8b66b8b7b462be1\|7582589cc4b7c656489f506056c6cc3db4e2f72a\|a5809e68bb2326010d13e32bc025ddce650934c8\|bcb0b8de41ba7223fc12fd66f8b66b8b7b462be1\)"
 
-git stash > /dev/null 2>&1
+stashed=false
+if git diff | grep . > /dev/null; then
+  git stash #> /dev/null 2>&1
+  stashed=true
+fi
+
 for typ in depute senateur; do
   fil="data/liste_${typ}s_collaborateurs.csv"
   ofil=$(echo $fil | sed 's/\.csv/-totaux\.csv/')
@@ -19,7 +24,9 @@ for typ in depute senateur; do
     done | uniq --check-chars=10 >> $ofil.tmp
   git checkout master > /dev/null 2>&1
 done
-git stash pop > /dev/null 2>&1
+if $stashed; then
+  git stash pop #> /dev/null 2>&1
+fi
 for typ in depute senateur; do
   mv data/liste_${typ}s_collaborateurs-totaux.csv{.tmp,}
 done
--- bin/update.sh
+++ bin/update.sh
@@ -89,7 +89,7 @@ fi
 
 if $gitpush; then
   bin/backlog_revisions.sh
-  if git status | grep "data/liste*-totaux.csv" > /dev/null; then
+  if git status | grep "data/liste.*-totaux.csv" > /dev/null; then
     git commit data/liste*-totaux.csv -m "update totaux" >> /tmp/update_collabs.tmp
   fi
   git push >> /tmp/update_collabs.tmp
