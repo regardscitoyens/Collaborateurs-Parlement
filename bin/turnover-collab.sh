191fca3a42c61c33bf1ebf95f26f7d6bb65abae8
diegantobass <diego.antolinosbasso@sciencespo.fr>
2018-06-27 14:35:45 +0200

    debug update previousscript

--- bin/turnover-collab.sh
+++ bin/turnover-collab.sh
@@ -5,7 +5,8 @@ git log data/liste_deputes_collaborateurs.csv  | grep -B 100000 0257cc2329695286
 PREVIOUSCOMMIT=$(tail -n 1 data/turnover-collab.commits.csv.tmp);
 tac data/turnover-collab.commits.csv.tmp | while read COMMIT ; do
         DATECOMMIT=$(git log $COMMIT  | grep Date: | head -n 1 | sed 's/Date: *//' )
-	git diff $PREVIOUSCOMMIT $COMMIT data/liste_deputes_collaborateurs.csv | grep '^[-+]"' | sed 's/^\+/"AJOUT",/' | sed 's/^-/"SUPPRESSION",/' | sed "s/$/,\"$DATECOMMIT\"/" 
+	git diff $PREVIOUSCOMMIT $COMMIT data/liste_deputes_collaborateurs.csv | grep '^[-+]"' | sed 's/^\+/"AJOUT",/' | sed 's/^-/"SUPPRESSION",/' | sed "s/$/,\"$DATECOMMIT\"/"
+	PREVIOUSCOMMIT=$COMMIT 
 done > data/turnover-collab.ajoutsuppression.csv.tmp
 
 python bin/collab_mouvements.py data/turnover-collab.ajoutsuppression.csv.tmp
