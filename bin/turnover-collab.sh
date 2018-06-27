579fdf860dc2ad5a9adfd3072ba46ecba5316f2e
Tangui Morlier <tangui@tangui.eu.org>
2018-06-27 12:09:47 +0200

    Génération d'un csv avec les dates d'ajout et de suppression de chaque collaborateur

---
+++ bin/turnover-collab.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git checkout master
+git log data/liste_deputes_collaborateurs.csv  | grep -B 100000 40a32ae027452ef2a41072268440e45b174f2077 | grep commit | sed 's/commit //'  > data/turnover-collab.commits.csv.tmp
+
+PREVIOUSCOMMIT=$(tail -n 1 data/turnover-collab.commits.csv.tmp);
+tac data/turnover-collab.commits.csv.tmp | while read COMMIT ; do
+        DATECOMMIT=$(git log 7cd556d9162b9d2e38a217ba84fbf9fb94df77c1  | grep Date: | head -n 1 | sed 's/Date: *//' )
+	git diff $COMMIT $PREVIOUSCOMMIT data/liste_deputes_collaborateurs.csv | grep '^[-+]' | sed 's/^\+/"AJOUT",/' | sed 's/^-/"SUPPRESSION",/' | sed "s/$/,\"$DATECOMMIT\"/" > data/tunover-collab.ajoutsuppression.csv.tmp
+done
+
+python bin/collab_mouvements.py data/tunover-collab.ajoutsuppression.csv.tmp
