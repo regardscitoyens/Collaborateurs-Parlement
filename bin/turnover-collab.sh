530b99d7dada980cf80853efec6aad4f65bd44ce
Tangui Morlier <tangui@tangui.eu.org>
2018-06-27 14:45:21 +0200

    Gestion des dates au format ISO

--- bin/turnover-collab.sh
+++ bin/turnover-collab.sh
@@ -4,7 +4,7 @@ git log data/liste_deputes_collaborateurs.csv  | grep -B 100000 0257cc2329695286
 
 PREVIOUSCOMMIT=$(tail -n 1 data/turnover-collab.commits.csv.tmp);
 tac data/turnover-collab.commits.csv.tmp | while read COMMIT ; do
-        DATECOMMIT=$(git log $COMMIT  | grep Date: | head -n 1 | sed 's/Date: *//' )
+        DATECOMMIT=$(git log --date=iso $COMMIT  | grep Date: | head -n 1 | sed 's/Date: *//' )
 	git diff $PREVIOUSCOMMIT $COMMIT data/liste_deputes_collaborateurs.csv | grep '^[-+]"' | sed 's/^\+/"AJOUT",/' | sed 's/^-/"SUPPRESSION",/' | sed "s/$/,\"$DATECOMMIT\"/" 
 done > data/turnover-collab.ajoutsuppression.csv.tmp
 
