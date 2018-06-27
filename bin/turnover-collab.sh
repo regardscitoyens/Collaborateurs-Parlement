2fb74244e3a8e3b63ebee4250547f5239a7ee602
Tangui Morlier <tangui@tangui.eu.org>
2018-06-27 12:24:07 +0200

    Coquille

--- bin/turnover-collab.sh
+++ bin/turnover-collab.sh
@@ -6,6 +6,6 @@ PREVIOUSCOMMIT=$(tail -n 1 data/turnover-collab.commits.csv.tmp);
 tac data/turnover-collab.commits.csv.tmp | while read COMMIT ; do
         DATECOMMIT=$(git log 7cd556d9162b9d2e38a217ba84fbf9fb94df77c1  | grep Date: | head -n 1 | sed 's/Date: *//' )
 	git diff $COMMIT $PREVIOUSCOMMIT data/liste_deputes_collaborateurs.csv | grep '^[-+]' | sed 's/^\+/"AJOUT",/' | sed 's/^-/"SUPPRESSION",/' | sed "s/$/,\"$DATECOMMIT\"/" 
-done > data/tunover-collab.ajoutsuppression.csv.tmp
+done > data/turnover-collab.ajoutsuppression.csv.tmp
 
-python bin/collab_mouvements.py data/tunover-collab.ajoutsuppression.csv.tmp
+python bin/collab_mouvements.py data/turnover-collab.ajoutsuppression.csv.tmp
